da74bbeed8cbdb15838a963fd9c4ab349d70160a
tikendraw <tikendraksahu1029@gmail.com>
2024-03-04 13:09:18 +0530

    init added

---
+++ src/core/news_api/__init__.py
@@ -0,0 +1,3 @@
+from .base_api import NewsAPI
+
+__all__ = ['NewsAPI']
--- src/core/news_api/base_api.py
+++ src/core/news_api/base_api.py
@@ -1,14 +1,18 @@
 from datetime import datetime
 from typing import List, Optional
 
+import requests
+
+from ..news_model import ArticlesResponse, NewsArticle
+
 
 class NewsAPI:
     def __init__(self, base_url: str) -> None:
         self.base_url = base_url
         
 
-    def get_news(self, query: str, page: int = 1) -> Optional[ArticlesResponse]:
-        response = requests.get(url)
+    def get_news(self, query: str, page: int = 1, *args, **kwargs) -> Optional[ArticlesResponse]:
+        response = requests.get(url, *args, **kwargs)
 
         return parse_news(response.json()) if response.status_code == 200 else None
 
