1a451de173dbd4cf56f3b1dac0506cd5e762bc05
tikendraw <tikendraksahu1029@gmail.com>
2024-03-04 21:26:02 +0530

    removed path print

--- src/core/db/__init__.py
+++ src/core/db/__init__.py
@@ -3,8 +3,6 @@ import sys
 from datetime import datetime
 from pathlib import Path
 
-# sys.path.append(str(Path(__file__).parent.parent.parent.absolute()))
-print(sys.path)
 from icecream import ic
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -13,6 +11,6 @@ from sqlalchemy.orm import Mapped, mapped_column, sessionmaker
 from .news_table import Base, NewsArticleORM
 
 # Create SQLite engine and session
-engine = create_engine('sqlite:///database/articles.db')
+engine = create_engine("sqlite:///database/articles.db")
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
