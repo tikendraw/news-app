b1de883cfb01c14de2b9efa25644941721504603
tikendraw <tikendraksahu1029@gmail.com>
2024-03-05 22:38:54 +0530

    added more fields

--- src/core/db/news_table.py
+++ src/core/db/news_table.py
@@ -4,17 +4,19 @@ from sqlalchemy.orm import Mapped, declarative_base, mapped_column
 # Define SQLAlchemy ORM model
 Base = declarative_base()
 
+
 class NewsArticleORM(Base):
-    __tablename__ = 'news_articles'
+    __tablename__ = "news_articles"
 
     # id = Column(Integer, primary_key=True)
     id: Mapped[int] = mapped_column(primary_key=True)
-
+    published_at = Column(DateTime)
     title = Column(String)
+    author = Column(String)
+    category = Column(String)
     description = Column(String)
     content = Column(String)
     url = Column(String)
     image = Column(String)
-    published_at = Column(DateTime)
     source_name = Column(String)
     source_url = Column(String)
