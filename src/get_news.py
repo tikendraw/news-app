30c0aa5d0b91387fb304d71a007534bbe6d62f75
tikendraw <tikendraksahu1029@gmail.com>
2024-03-05 22:49:10 +0530

    test newly added NewsDataAPI api

--- src/get_news.py
+++ src/get_news.py
@@ -1,17 +1,36 @@
 import os
 
 from dotenv import load_dotenv
+from icecream import ic
 
 from core.db import Session
 from core.db.db_utils import *
-from core.news_api import GNewsAPI
+from core.news_api import GNewsAPI, NewsDataAPI
 
 load_dotenv()
 
-apikey = os.environ.get("GNEWS_API_KEY")
-test = GNewsAPI(category="general", n_news=100, lang="en", country="us", apikey=apikey)
+# gnews_apikey = os.environ.get("GNEWS_API_KEY")
+# gnews = GNewsAPI(category="general", n_news=100, lang="en", country="us", apikey=gnews_apikey)
 
-news = test.get_news()
+# news = gnews.get_news()
 
-# print(news)
-add_articles_to_db(session=Session(), articles=news)
+# # print(news)
+# add_articles_to_db(session=Session(), articles=news)
+
+newsdata_apikey = os.environ.get("NEWSDATA_API_KEY")
+newsdata = NewsDataAPI(apikey=newsdata_apikey)
+
+response = newsdata.get_news(country="jp")
+print(response)
+
+# response = newsdata.get_news(country='jp')
+# print(response)
+# response = newsdata._make_request(country='cn')
+# response = response['results'][0]
+# for i, j in response.items():
+#     # print(i,'===', type(j))
+
+# for i in response:
+#     print(i['title'])
+#     print(i['link'])
+#     print()
