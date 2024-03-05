737186a27094070aebc4757aee005f9bc66d48dc
tikendraw <tikendraksahu1029@gmail.com>
2024-03-05 22:44:27 +0530

    NewsDataAPI added

--- src/core/news_api/__init__.py
+++ src/core/news_api/__init__.py
@@ -1,4 +1,5 @@
 from .base_api import NewsAPI
 from .gnews_api import GNewsAPI
+from .newsdata_api import NewsDataAPI
 
-__all__ = ["NewsAPI", "GNewsAPI"]
+__all__ = ["NewsAPI", "GNewsAPI", "NewsDataAPI"]
