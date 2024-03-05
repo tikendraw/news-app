312128d67807dcfd1d48159edeb6bddb870b8c5a
tikendraw <tikendraksahu1029@gmail.com>
2024-03-05 22:48:06 +0530

    removed source class, added more fields

--- src/core/news_model/news.py
+++ src/core/news_model/news.py
@@ -1,23 +1,24 @@
 from datetime import datetime
-from typing import List, Optional
+from typing import List, Optional, Union
 
 from pydantic import BaseModel, Field, HttpUrl
 
 
-class Source(BaseModel):
-    name: str = None
-    url: HttpUrl = None
-
 class NewsArticle(BaseModel):
     title: str
     description: Optional[str] = None
-    content: Optional[str]= None
-    url: Optional[HttpUrl]= None
-    image: Optional[HttpUrl]= None
+    category: Optional[str] = None
+    author: Optional[str] = None
+    content: Optional[str] = None
+    country: Optional[list[str]] = None
+    language: Optional[list[str]] = None
+    url: Optional[HttpUrl] = None
+    image: Optional[HttpUrl] = None
     published_at: Optional[datetime] = Field(alias="publishedAt", default=None)
-    source: Optional[Source]
+    source_name: Optional[str] = None
+    source_url: Optional[Union[HttpUrl, str]] = None
+
 
 class ArticlesResponse(BaseModel):
     totalArticles: int
     articles: List[NewsArticle]
-
