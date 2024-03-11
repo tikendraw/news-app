ccc68cfbf31b2d628c7d7737bf45699ddf36c270
tikendraw <tikendraksahu1029@gmail.com>
2024-03-11 14:53:38 +0530

    deleted

--- src/core/news_model/__init__.py
+++
@@ -1,6 +0,0 @@
-from .news import NewsArticle
-from .newsdata_article import NewsDataArticle
-from .gnews_article import GNewsArticle
-from .googlenews_article import GoogleNewsArticle
-
-__all__ = ["NewsArticle", "GNewsArticle", "NewsDataArticle", "GoogleNewsArticle"]
