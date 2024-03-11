b307e34fb9611b99ee52f43f5356fdcc91e43316
tikendraw <tikendraksahu1029@gmail.com>
2024-03-11 15:00:31 +0530

    checking things

---
+++ src/test_db.py
@@ -0,0 +1,19 @@
+from icecream import ic
+
+from core.db import Session
+from core.db.news_tables import NewsArticleORM, ScienceArticleORM
+from core.news_schema import NewsArticle
+
+news = NewsArticle(
+    title="scienc",
+    content="test science",
+)
+
+from core.db.db_utils import *
+
+db = next(get_db())
+
+# a = add_article_to_db(news, db, ScienceArticleORM)
+# print(a)
+
+a = delete_article_from_db(news, db, ScienceArticleORM)
