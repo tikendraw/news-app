8f52b83b504996b122c8524fb2a0c055a165748b
soumyajit <soumyajitsahu2013rta@gmail.com>
2024-03-13 01:10:15 +0530

    data ingestion notebook

--- src/core/utils/common.py
+++ src/core/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from src.logging import logger
+from src.core.logging import logger
 from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
