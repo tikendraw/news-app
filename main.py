c4a28df8395a6d4de59e8a1e6f9a427cd4d9c37b
soumyajit <soumyajitsahu2013rta@gmail.com>
2024-03-14 11:57:00 +0530

    model evaluation notebook and pipeline componenets

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.core.pipeline.one_01_data_ingestion import DataIngestionTrainingPipelin
 from src.core.pipeline.two_02_data_validation import DataValidationTrainingPipeline
 from src.core.pipeline.three_03_data_transformation import DataTransformationTrainingPipeline
 from src.core.pipeline.four_04_model_trainer import ModelTrainerTrainingPipeline
+from src.core.pipeline.five_05_model_evaluation import ModelEvaluationTrainingPipeline
 from src.core.logging import logger
 
 
@@ -44,6 +45,19 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    model_trainer = ModelTrainerTrainingPipeline()
    model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Model Evaluation stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evaluation = ModelEvaluationTrainingPipeline()
+   model_evaluation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
--- src/core/config/configuration.py
+++ src/core/config/configuration.py
@@ -1,6 +1,6 @@
 from src.core.constants import *
 from src.core.utils.common import read_yaml, create_directories
-from src.core.entity import (DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig)
+from src.core.entity import (DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig,ModelEvaluationConfig)
 
 
 class ConfigurationManager:
@@ -81,3 +81,22 @@ class ConfigurationManager:
         )
 
         return model_trainer_config
+    
+
+
+
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config = self.config.model_evaluation
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            model_path = config.model_path,
+            tokenizer_path = config.tokenizer_path,
+            metric_file_name = config.metric_file_name
+           
+        )
+
+        return model_evaluation_config
