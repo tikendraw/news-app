c73be3fd14a33ebb331113802e53160c2e4784b0
soumyajit <soumyajitsahu2013rta@gmail.com>
2024-03-14 06:58:56 +0530

    data validation notebook and pipline all parts

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.core.pipeline.one_01_data_ingestion import DataIngestionTrainingPipeline
 from src.core.pipeline.two_02_data_validation import DataValidationTrainingPipeline
+from src.core.pipeline.three_03_data_transformation import DataTransformationTrainingPipeline
 from src.core.logging import logger
 
 
@@ -19,6 +20,17 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_validation = DataValidationTrainingPipeline()
    data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_transformation = DataTransformationTrainingPipeline()
+   data_transformation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
---
+++ src/core/pipeline/three_03_data_transformation.py
@@ -0,0 +1,14 @@
+from src.core.config.configuration import ConfigurationManager
+from src.core.components.data_transformation import DataTransformation
+from src.core.logging import logger
+
+
+class DataTransformationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_transformation_config = config.get_data_transformation_config()
+        data_transformation = DataTransformation(config=data_transformation_config)
+        data_transformation.convert()
