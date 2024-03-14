449cd785e545e9f74fb45a4714ad5988c8626bc3
tikendraw <tikendraksahu1029@gmail.com>
2024-03-14 12:15:01 +0530

    renamed to source apis

---
+++ src/core/sources/apis/__init__.py
@@ -0,0 +1,6 @@
+from .base_api import NewsAPI
+from .gnews_api import GNewsAPI
+from .googlenews_api import GoogleNewsAPI
+from .newsdata_api import NewsDataAPI
+
+__all__ = ["NewsAPI", "GNewsAPI", "NewsDataAPI", "GoogleNewsAPI"]
