1b6aba56cf30a0da91e46da84980ff15696f0756
soumyajit <soumyajitsahu2013rta@gmail.com>
2024-03-14 06:28:20 +0530

    data validation notebook and pipeline components

--- src/core/config/configuration.py
+++ src/core/config/configuration.py
@@ -1,10 +1,8 @@
 from src.core.constants import *
 from src.core.utils.common import read_yaml, create_directories
-from src.core.entity import (DataIngestionConfig)
+from src.core.entity import (DataIngestionConfig,DataValidationConfig)
 
-from pathlib import Path
-CONFIG_FILE_PATH = Path("config\config.yaml")
-PARAMS_FILE_PATH = Path("params.yaml")
+
 class ConfigurationManager:
     def __init__(
         self,
@@ -31,3 +29,17 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            ALL_REQUIRED_FILES=config.ALL_REQUIRED_FILES,
+        )
+
+        return data_validation_config
