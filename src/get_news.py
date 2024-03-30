1fd76e61b9bba6e2bb84f1d0b674beca0d6582dd
tikendraw <tikendraksahu1029@gmail.com>
2024-03-30 17:51:57 +0530

    import path fixed

--- src/get_news.py
+++ src/get_news.py
@@ -1,7 +1,14 @@
 import json
 import os
+import sys
 from datetime import datetime
+from pathlib import Path
 
+root_dir=Path(__file__).parent.parent.absolute()
+
+if root_dir.name=='news-app':
+    sys.path.append(str(root_dir))
+    
 from dotenv import load_dotenv
 from icecream import ic
 
