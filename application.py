ddaf46cca26221ce24541d843d090249841c0b8a
soumyajit <soumyajitsahu2013rta@gmail.com>
2024-03-31 11:30:33 +0530

    some changes

--- application.py
+++ application.py
@@ -1,19 +1,38 @@
-from flask import Flask, request, render_template
-from src.core.pipeline.prediction import PredictionPipeline  # Import your pipeline class
+from flask import Flask, render_template
+from src.prediction import PredictionPipeline  # Import your pipeline class
+import json
 
 app = Flask(__name__)
 
-pipeline = PredictionPipeline()  # Instantiate the pipeline
+pipeline = PredictionPipeline()  # Intiate the pipeline
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')  # Render the HTML template
+    return render_template("index.html")  # Render the HTML template
 
-@app.route('/summarize', methods=['POST'])
+
+# @app.route('/summarize', methods=['POST'])
+# def summarize():
+#     text = request.form['text']
+#     summary = pipeline.predict(text)
+#     return render_template('result.html', summary=summary)
+
+
+@app.route("/summarize", methods=["POST"])
 def summarize():
-    text = request.form['text']
-    summary = pipeline.predict(text)
-    return render_template('result.html', summary=summary)
+    # Load news data from gnews.json
+    with open("gnews.json", "r") as f:
+        news_data = json.load(f)
+    articles = news_data["articles"]
+
+    article_summaries = []
+    for article in articles:
+        summary = pipeline.predict(article["content"])
+        article_summaries.append({"title": article["title"], "summary": summary})
+
+    return render_template("result.html", article_summaries=article_summaries)
+
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080)
