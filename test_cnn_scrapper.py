b911d95c3cbfa994b1c050a2e7b636366ab14077
tikendraw <tikendraksahu1029@gmail.com>
2024-04-03 22:49:57 +0530

    trying langchain to summarize article

--- test_cnn_scrapper.py
+++ test_cnn_scrapper.py
@@ -2,16 +2,26 @@ from core.db.db_utils import get_db
 from core.db.news_tables import NewsArticleORM
 from core.scrapper.cnn_scrapper import CNNScraper
 from core.db.crud.news_crud import add_article
+from core.langchain_summariezer import summary_chain
 
 cnn_scapper = CNNScraper(enable_cache=True)
-articles = cnn_scapper.run(n=10)
+articles = cnn_scapper.run(n=3)
 
-def you_summarier_function(content:str) -> str:
-    ...
+
     
 for num,article in enumerate(articles):
-    article.content_summary = your_summarier_function(article.content)
+    article_summary = summary_chain(article.content)
+    article.content_summary = article_summary.content_summary
+    article.locations = article_summary.locations
+    article.keywords = article_summary.tags
+    article.category = article_summary.category
+    
+    
     add_article(article=article, db=next(get_db()), orm_class=NewsArticleORM )
+    
+    print(f"{num}:  {article.title}")
+    print("ai_tittle: ", article_summary.title)
+    print()
     
 # cnn_scapper.write_db(session=next(get_db()), orm_class=NewsArticleORM)
 print("Done!!!")
