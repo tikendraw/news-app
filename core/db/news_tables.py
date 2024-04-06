c263152068965e70fadb3b809e9da8bdd1199685
tikendraw <tikendraksahu1029@gmail.com>
2024-04-06 13:36:21 +0530

    added insert event,  changed columns names

--- core/db/news_tables.py
+++ core/db/news_tables.py
@@ -1,8 +1,10 @@
 from typing import Any, Dict, List
+from sqlalchemy.orm import Session
 
-from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy import JSON, Boolean, Column, ForeignKey, String
 from sqlalchemy.dialects.mysql import JSON
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import event
 
 
 class Base(DeclarativeBase):
@@ -15,13 +17,20 @@ class NewsArticleSummaryORM(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     news_article_id: Mapped[int] = mapped_column(ForeignKey("news_articles.id"))
-    summary_column: Mapped[str] = Column(String)
+    ai_title: Mapped[str] = Column(String, nullable=True)
+    summary: Mapped[str] = Column(String)
     tags: Mapped[List[str]] = mapped_column(JSON, nullable=True)
     locations: Mapped[List[str]] = mapped_column(JSON, nullable=True)
-    ai_title: Mapped[str] = Column(String, nullable=True)
+    category: Mapped[List[str]] = mapped_column(JSON, nullable=True)
 
     news_article = relationship("NewsArticleORM", back_populates="summary")
-    
+
+@event.listens_for(NewsArticleSummaryORM, 'after_insert')
+def set_is_summarized(mapper, connection, target):
+    with Session(connection) as session:
+        article = session.query(NewsArticleORM).get(target.news_article_id)
+        article.is_summarized = True
+        session.commit()
 
 class NewsArticleORM(Base):
     __tablename__ = "news_articles"
@@ -33,7 +42,6 @@ class NewsArticleORM(Base):
     category: Mapped[List[str]] = mapped_column(JSON, nullable=True)
     description = Column(String)
     content = Column(String)
-    content_summary = Column(String)
     url = Column(String)
     images: Mapped[List[str]] = mapped_column(JSON, nullable=True)
     source_name = Column(String)
@@ -42,4 +50,5 @@ class NewsArticleORM(Base):
     keywords: Mapped[List[str]] = mapped_column(JSON, nullable=True)
 
     summary = relationship("NewsArticleSummaryORM", back_populates="news_article")
-    
+    is_summarized: Mapped[bool] = Column(Boolean, default=False)
+        
